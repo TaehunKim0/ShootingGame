125a32d5797a93f935883fac1fb4865bd75e13e3
TaehunKim0 <jack071000@naver.com>
2023-07-11 13:12:06 +0900

    first

---
+++ ShootingGame/Assets/Projectile/Script/ProjectileMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public float Movespeed = 2f;
+
+    public GameObject ExplodeFX;
+
+    [SerializeField]
+    private float LifeTime = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(Movespeed * Time.deltaTime, 0, 0));
+    }
+
+    private void OnDestroy()
+    {
+        //Instantiate(ExplodeFX, transform.position, Quaternion.identity);
+    }
+}
