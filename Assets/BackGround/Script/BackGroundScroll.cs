9e9e7df8bd0b9e4ba0603a53e2e14cff5176b57f
TaehunKim0 <jack071000@naver.com>
2023-07-11 13:50:00 +0900

    second commit

---
+++ Assets/BackGround/Script/BackGroundScroll.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class BackGroundScroll : MonoBehaviour
+{
+    public float ScrollSpeed;
+    public Transform[] Backgrounds;
+
+    private float _leftPosX;
+    private float _imageWidth;
+
+    void Start()
+    {
+        _imageWidth = Backgrounds[0].GetComponent<SpriteRenderer>().size.x;
+        _leftPosX = Backgrounds[0].position.x - _imageWidth;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < Backgrounds.Length; i++)
+        {
+            if (null == Backgrounds[i])
+                continue;
+
+            Backgrounds[i].position += new Vector3(-ScrollSpeed, 0, 0) * Time.deltaTime;
+
+            if (Backgrounds[i].position.x < _leftPosX)
+            {
+                int index = i - 1;
+                if (index < 0)
+                {
+                    index = Backgrounds.Length - 1;
+                }
+
+                Backgrounds[i].position = new Vector3(Backgrounds[index].position.x + _imageWidth, 0, 0);
+            }
+        }
+    }
+}
